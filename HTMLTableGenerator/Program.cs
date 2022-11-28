ebb42d79f317b69d4dd953f25203efffc9e476ae
Selkan Larlar Trainer <sl@trainer.com>
2022-11-28 13:43:44 +0300

    html tablo algoritma planlandi

--- HTMLTableGenerator/Program.cs
+++ HTMLTableGenerator/Program.cs
@@ -6,7 +6,23 @@ string moviesText = File.ReadAllText("movies.json");
 
 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 List<Movie> movies = JsonSerializer.Deserialize<List<Movie>>(moviesText, options);
-foreach (var m in movies)
-{
-    Console.WriteLine(m.Title);
-}
+
+//foreach (var m in movies)
+//{
+//    Console.WriteLine(m.Title);
+//}
+
+// Tablo başla
+string tabloBasla = "";
+// Tablo başlıklar
+string tabloBasliklar = "";
+// Tablo gövde
+string tabloGovde = "";
+// Tablo kapat
+string tabloKapat = "";
+
+string tablo = tabloBasla + tabloBasliklar + tabloGovde + tabloKapat;
+File.WriteAllText("output.html", tablo);
+
+
+
