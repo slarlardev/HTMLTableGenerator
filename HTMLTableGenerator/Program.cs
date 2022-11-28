3286f019bb34aceb3bd6ff45d1e79f668e26ba1e
Selkan Larlar Trainer <sl@trainer.com>
2022-11-28 13:14:58 +0300

    deserialize tamamlandı

--- HTMLTableGenerator/Program.cs
+++ HTMLTableGenerator/Program.cs
@@ -1,2 +1,12 @@
-﻿string moviesText = File.ReadAllText("movies.json");
-Console.WriteLine(moviesText);
+﻿using HTMLTableGenerator;
+using System.Text.Json;
+
+string moviesText = File.ReadAllText("movies.json");
+//Console.WriteLine(moviesText);
+
+var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+List<Movie> movies = JsonSerializer.Deserialize<List<Movie>>(moviesText, options);
+foreach (var m in movies)
+{
+    Console.WriteLine(m.Title);
+}
